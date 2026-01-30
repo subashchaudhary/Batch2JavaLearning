b343a1828eea7492e22f9734209696c21f1106c3
subashchaudhary <subashtharu116@gmail.com>
2026-01-30 10:36:20 +0545

    wrapper classes

---
+++ wrapper/WrapperDemo.java
@@ -0,0 +1,27 @@
+package wrapper;
+
+public class WrapperDemo {
+
+
+    static void main() {
+
+
+        int num = 1;
+
+
+        //AutoBoxing
+        Integer num3 = num;  //AutoBoxing
+
+        Integer num4 = (Integer) num;
+
+       System.out.println(num);
+
+
+       //Unboxing
+        int newNum = num3;
+
+
+
+
+    }
+}
